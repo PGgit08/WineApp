7036f0431892d49dddf555eb348be4d1d7aeb33b
PGgit08 <peter.mty8@gmail.com>
2020-12-05 20:14:36 -0500

    add blueprints and shit

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+# base dir for creation uri for database
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# create uri
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+
+# create app
+app = Flask(__name__)
+
+# set config for app 
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import models
